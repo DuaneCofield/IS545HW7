c7b39b2e75f0b433bb77dffc8da68fd90e124655
Duane Cofield <cofieldd@gmail.com>
2013-11-24 17:48:51 -0800

    Original Commit

---
+++ HW7/HW7/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HW7.Startup))]
+namespace HW7
+{
+    public partial class Startup 
+    {
+        public void Configuration(IAppBuilder app) 
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
